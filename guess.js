ac438d5cf0fe8e6c0d1f22786361ab8498bf6a6a
blogerr <40164963+blogerr@users.noreply.github.com>
2018-06-11 14:31:10 +0530

    Create guess.js

---
+++ guess.js
@@ -0,0 +1,20 @@
+var count = 0;
+var vc = 0;
+while (count < 1) {
+    document.write(makeid() + "\n");
+    vc++;
+    if (vc == 100000) {
+        count = 1;
+    }
+
+}
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
